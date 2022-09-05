b6f106e7c7bb7b2cb642aac3d3b3189c9086b14f
Maryll Castelino <maryllcastelino@gmail.com>
2022-09-06 00:31:38 +0530

    Added styling
    Added styling for home page
    Added @tailwindcss/typography as dependency and used prose class on markdown content

--- frontend/pages/blog/[blogId].js
+++ frontend/pages/blog/[blogId].js
@@ -9,9 +9,8 @@ export const getStaticPaths = () => {
           blogId: "abcdef",
         },
       },
-    
     ],
-    fallback: true
+    fallback: true,
   };
 };
 
@@ -25,23 +24,28 @@ export const getStaticProps = async ({ params }) => {
     props: {
       post: data || null,
     },
-    revalidate: 3
+    revalidate: 3,
   };
 };
 
-const BlogPost = ({post}) => {
+const BlogPost = ({ post }) => {
   const router = useRouter();
   const { blogId } = router.query;
 
-  console.log({ router });
-
-  if(router.isFallback) {
-      return <h1>Loading...</h1>
+  if (router.isFallback) {
+    return <h1>Loading...</h1>;
   }
 
   return (
-    <div>
-      <h1>{post.title}</h1>
+    <div className="py-16 px-6 sm:px-8">
+      <header>
+        <h1 className="flex flex-col items-center">
+          <span className="mt-2 text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">{post.title}</span>
+        </h1>
+      </header>
+      <div className="mt-8 mx-auto prose md:prose-lg lg:prose-xl">
+        <ReactMarkdown children={post.body}></ReactMarkdown>
+      </div>
     </div>
   );
 };
