7443b9e9c878091154266436ef195abccc85fe9a
Maryll Castelino <maryllcastelino@gmail.com>
2023-05-06 10:22:39 +0530

    removed link underline and added gradient heading

--- components/blog-card.js
+++ components/blog-card.js
@@ -12,13 +12,13 @@ const BlogCard = (post) => {
 
   return (
     <Link href={`/blog/${number}`}>
-      <a className="block p-6 m-10 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
+      <div className="block p-6 m-10 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 underline-none">
           {title}
         </h5>
         <p className="font-normal text-gray-700 ">{formattedDate}</p>
         <a className="text-gray-800">Read More...</a>
-      </a>
+      </div>
     </Link>
   );
 };
